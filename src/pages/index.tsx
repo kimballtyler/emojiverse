07a1f2df2fc30b8ec93947f1c9514d35e50c1156
Tyler Kimball <tkimball@dexwell.com>
2023-04-17 10:34:41 -0700

    back and front make a post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,22 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
 
   const { user } = useUser();
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      ctx.posts.getAll.invalidate();
+    }
+  });
 
   if (!user) return null;
 
@@ -26,7 +36,14 @@ const CreatePostWizard = () => {
       width={56}
       height={56}
     />
-    <input placeholder="Type emojis!" className="bg-transparent outline-none grow" />
+    <input
+      placeholder="Type emojis!"
+      className="bg-transparent outline-none grow"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      disabled={isPosting}
+    />
+    <button onClick={() => mutate({ content: input })}>Post</button>
   </div>
 }
 
@@ -64,7 +81,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-      {data?.map(fullPost => (
+      {data.map(fullPost => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
