ee40ea2bf1e86603ab2987b3aa39bc5dee820b1e
Tyler Kimball <tkimball@dexwell.com>
2023-04-28 08:49:06 -0700

    profile page and ssg helper

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,8 +5,8 @@ export const PageLayout = (props: PropsWithChildren) => {
 
 
     return (
-        <main className="flex justify-center h-screen">
-            <div className="w-full md:max-w-2xl border-x border-slate-400 overflow-y-scroll">
+        <main className="overflow-none flex h-screen justify-center">
+            <div className="flex h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
                 {props.children}
             </div>
         </main>
