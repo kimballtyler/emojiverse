8a2d995b497c7c99d47abebbce20435fccca4286
Tyler Kimball <tkimball@dexwell.com>
2023-04-28 10:08:18 -0700

    profile page posts

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,6 +4,18 @@ import { api } from "~/utils/api";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
 
+const ProfileFeed = (props: { userId: sting }) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({ userId: props.userId })
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0) return <div>User has not posted</div>;
+
+  return <div className="flex flex-col">
+    {data.map(fullPost => (<PostView {...fullPost} key={fullPost.post.id} />))}
+  </div>
+}
+
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
     username,
@@ -28,6 +40,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <div className="h-[64px]"></div>
         <div className="p-4 text-2xl font-bold">{`@${data.username ?? ''}`}</div>
         <div className="border-b border-slate-400 w-full"></div>
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
@@ -37,6 +50,8 @@ import { createServerSideHelpers } from '@trpc/react-query/server';
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postview";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,15 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Link from "next/link";
 
-import { type RouterOutputs, api } from "~/utils/api";
+import { type api } from "~/utils/api";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 
-dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
 
@@ -73,35 +70,6 @@ const CreatePostWizard = () => {
     </div>
   );
 };
-
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number]
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-  return (
-    <div key={post.id} className="flex border-b border-slate-400 p-4 gap-3">
-      <Image
-        src={author.profileImageUrl}
-        className="rounded-full w-14 h-14"
-        alt={`@${author.username}'s profile picture`}
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username} `}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
-          </Link>
-        </div>
-        <span className="text-2xl">{post.content}</span>
-      </div>
-    </div>
-  )
-}
 
 const Feed = () => {
   const { data, isLoading: postsloading } = api.posts.getAll.useQuery();
